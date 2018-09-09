9f1b453ac079881c913f1629281d64c848f36095
Anthony Bedford <tony.bedford@live.co.uk>
2018-09-09 17:54:14 +0100

    More tidy up.

--- c/server.c
+++ c/server.c
@@ -1,71 +1,74 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
-#define PORT 8080 
+#define PORT 8080
+#define BUF_SZ 1024
 
-int main(int argc, char const *argv[]) 
-{ 
+
+int main(int argc, char const *argv[])
+{
     int server_fd, new_socket, valread; // socket is actually an integer
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Set socket options 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
+    char *hello = "Hello from server";
 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); // Convert port number from host byte order to short network byte order
-    memset (&address.sin_zero, 0, 8); // pad sockaddr_in to size of sockaddr  
-       
-    // Bind the socket 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
+    // set up address
+    struct sockaddr_in address; // IPv4 socket address type, cast to before use in socket function
+    int addrlen = sizeof(address);
+    memset(&address, 0, addrlen);         // clear struct for compatibility and pad sockaddr_in to size of sockaddr
+    address.sin_family = AF_INET;         // Address format internet, should match that used in socket creation
+    address.sin_addr.s_addr = INADDR_ANY; // this holds the actual IP address
+    address.sin_port = htons(PORT);       // Convert port number from host byte order to short network byte order
+
+    // Creating socket file descriptor - this uses TCP here (stream-based connection)
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the socket
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
 
     // Listen
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
     // Accept
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
-    // Blocking Read socket 
-    valread = read(new_socket, buffer, 1024); 
-    printf("%s\n",buffer ); 
-    
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // Blocking Read socket
+    char buffer[BUF_SZ] = {0};
+    valread = read(new_socket, buffer, BUF_SZ);
+    printf("%s\n", buffer);
+
     // Send on socket
-    send(new_socket, hello, strlen(hello), 0 ); 
-    printf("Hello message sent\n"); 
-    return 0; 
-} 
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    return 0;
+}
