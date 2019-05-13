704f59d43d338dc588aa572b6c408119d75c3d69
Tony Bedford <tony.bedford@vonage.com>
2019-05-13 13:47:28 +0100

    Add column with compounding and fix errors

--- python/fire-calc.py
+++ python/fire-calc.py
@@ -1,13 +1,19 @@
 core = 1000 # assume core expenses 1000 a month
-lump = 25 * core
+lump = 25 * 12 * core # 4% rule
+rate = 0.07
 
-def calc_gross_required (monthly):
-    gross = monthly * 12
-    gross = gross + (gross * 1/3) 
-    return int(gross)
-
+def calc_years(lump, saving):
+    y = 1
+    fv = (saving * 12) * (1 + rate)
+    while fv < lump:
+        y = y + 1
+        fv = (fv + (saving * 12)) * (1 + rate)
+    return y
+        
 print("---\nCore: %d\nLump sum required: %d\n---" % (core, lump))
-print("Gross\tNet\tSaving\tYears\tFireFactor")
-for m in range(1500, 6000, 500):
-    saving = m - core
-    print("%d\t%d\t%d\t%d\t%.1f" % (calc_gross_required(m), m, saving, int(300000/(saving * 12)), m/core))
+print("Gross\tMonthly\tNet\tSaving\tYears\tYears\tFireFactor")
+for gross in range(23000, 80000, 4000):
+    m_gross = gross / 12
+    m_net = m_gross * 2/3
+    m_saving = m_net - core
+    print("%d\t%d\t%d\t%d\t%.1f\t%d\t%.1f" % (gross, m_gross, m_net, m_saving, lump/(m_saving * 12), calc_years(lump, m_saving), m_gross/core))
