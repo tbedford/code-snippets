ffff24acd1f5595db04c48f3f0513bbdbfc1bea2
Tony Bedford <tony.bedford@vonage.com>
2019-10-31 16:22:09 +0000

    First version

--- python/slugify.py
+++ python/slugify.py
@@ -1,2 +1,10 @@
 # Generate a slug
 
+s1 = "  This is a title to slugify "
+print(s1)
+
+def slugify(title):
+    return title.strip().lower().replace(' ', '-')
+    
+print(">{slug}<".format(slug=slugify(s1)))
+
