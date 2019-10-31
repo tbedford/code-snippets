bde539ff7021410f70418fc2f8db2674ff75d717
Tony Bedford <tony.bedford@vonage.com>
2019-10-31 16:43:42 +0000

    Improved ability to handle non-word chars

--- python/slugify.py
+++ python/slugify.py
@@ -3,6 +3,10 @@ import re
 s1 = "  This! is a? title to slugify?! "
 print(s1)
 
+
+# Slight hack is to convert spaces to underscore first.  This allows
+# you just use '\W' to remove unwanted chars from string. You then
+# need to convert the underscores to '-'.
 def slugify(title):
     title = title.strip().lower().replace(' ', '_')
     title = re.sub(r'\W', '', title)
