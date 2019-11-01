605566d5184e7397b248f93647b3a6995583af83
Tony Bedford <tony.bedford@vonage.com>
2019-11-01 13:48:31 +0000

    Update

--- python/preproc-meta.py
+++ python/preproc-meta.py
@@ -2,7 +2,7 @@
 
 import re
 
-s = '''
+s1 = '''
 ---
 title: Understanding JWTs
 summary: >-
@@ -14,12 +14,35 @@ date_published: 2018-11-01T00:00:00.000Z
 date_updated: 2018-11-01T00:00:00.000Z
 date_test: 2018-11-01T12:34:56.789Z
 ---
+
+Stuff here...
 '''
 
-m = re.search(r'summary: >-([\s\S]*?)cat:', s, re.MULTILINE)
+s2 = '''
+---
+title: Understanding JWTs
+summary: This article takes a look at JSON Web Tokens or JWTs.
+cat: Code
+tags: 'python, jwt'
+date_published: 2018-11-01T00:00:00.000Z
+date_updated: 2018-11-01T00:00:00.000Z
+date_test: 2018-11-01T12:34:56.789Z
+---
 
-z = m.group(1).strip()
-z = z.replace('\n', '')
-z = z.replace('  ', ' ') 
+Stuff here...
+'''
 
-print(z)
+def fix_summary(s):
+    m = re.search(r'summary: >-([\s\S]*?)cat:', s, re.MULTILINE)
+    if m: 
+        z = m.group(1).strip()
+        z = z.replace('\n', '')
+        z = z.replace('  ', ' ')
+        f = "summary: {z}\ncat:".format(z=z)
+        x = re.sub(r'summary: >-[\s\S]*?cat:', f, s, 1) 
+        return x
+    else:
+        return s
+
+print(fix_summary(s1))
+print(fix_summary(s2))
