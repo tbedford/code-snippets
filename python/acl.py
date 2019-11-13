aca2745df8fafd4ca8a73c0986b2bcfa325b5ddc
Tony Bedford <tony.bedford@vonage.com>
2019-11-13 11:29:44 +0000

    New

---
+++ python/acl.py
@@ -0,0 +1,7 @@
+import json
+from pprint import pprint
+
+acl = '{"paths":{"/*/users/**":{},"/*/conversations/**":{},"/*/sessions/**":{},"/*/devices/**":{},"/*/image/**":{},"/*/media/**":{},"/*/applications/**":{},"/*/push/**":{},"/*/knocking/**":{}}}'
+
+print(acl)
+
