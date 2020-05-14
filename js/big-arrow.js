1987681976be75948a9fdd28da1cf0f57a1edc7e
Tony Bedford <tony.bedford@vonage.com>
2020-05-14 11:51:24 +0100

    Using big arrow

---
+++ js/big-arrow.js
@@ -0,0 +1,17 @@
+function myFunc1 () {
+    return 'myFunc1'
+}
+console.log(myFunc1())
+
+let myFunc2 = function() { return 'myFunc2' }
+console.log(myFunc2())
+
+let myFunc3 = () => { return 'myFunc3' }
+console.log(myFunc3())
+
+let myFunc4 = (s) => { return s }
+console.log(myFunc4('someString'))
+
+
+
+
