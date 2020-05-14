a161ae0d8461480868ac4bdc27d5b2ac1d466319
Tony Bedford <tony.bedford@vonage.com>
2020-05-14 17:08:06 +0100

    New

---
+++ js/promise-code.js
@@ -0,0 +1,23 @@
+let p1 = new Promise(function (resolve, reject) {
+
+    resolve('Promise 1 resolved')
+
+});
+
+let p2 = new Promise( (resolve, reject) => {
+
+    let status = true; // status of async op, hardcoded for testing 
+
+    if (status){
+        resolve('Promise resolved')
+    }
+    else {
+        reject('Promise rejected')
+    }
+});
+
+//p2.then( resolve => { console.log(resolve) }).catch( reject => console.error(reject) )
+
+p1.then( resolve => { console.log(resolve) }).then(resolve => p2)
+    .then( resolve => { console.log(resolve) })
+    .catch( reject => console.error(reject) )
